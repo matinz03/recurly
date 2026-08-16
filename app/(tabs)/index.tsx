60b5895b9682b62a27da1f858e85460c26fdc549
MATIN ZOMORRODABEDI <74014122+matinz03@users.noreply.github.com>
2026-08-16 16:59:08 +0200

    Update Spotify Subscription link path

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ export default function App() {
             <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign up</Link>
 
-            <Link href="/app/subscriptions/spotify">Spotify Subscription</Link>
+            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
             <Link
                 href={{
                     pathname: "/subscriptions/[id]",
