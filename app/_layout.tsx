8ecacdfe7e2a905d5c1cea7bc0f8b8afce6eae23
Matin <matinpsn1@gmail.com>
2026-08-17 12:27:59 +0200

    fix: apply parallel loading for clerk and fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,46 @@
-import {SplashScreen, Stack} from "expo-router";
-import '@/global.css';
-import {useFonts} from "expo-font";
-import {useEffect} from "react";
-import { ClerkProvider } from '@clerk/expo';
-import { tokenCache } from '@clerk/expo/token-cache';
+import { SplashScreen, Stack } from "expo-router";
+import "@/global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+import { ClerkProvider, useAuth } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 
 SplashScreen.preventAutoHideAsync();
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
-  throw new Error('Add your Clerk Publishable Key to the .env file');
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
+function RootLayoutContent() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+  const {isLoaded:authLoaded} = useAuth();
+
+  useEffect(() => {
+    if (fontsLoaded&&authLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, authLoaded]);
+
+  if (!fontsLoaded || !authLoaded) return null;
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
-    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
-    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
-    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
-    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
-    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
-  })
-
-  useEffect(() => {
-    if(fontsLoaded) {
-      SplashScreen.hideAsync()
-    }
-  }, [fontsLoaded])
-
-  if (!fontsLoaded) return null;
-
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <RootLayoutContent />
     </ClerkProvider>
   );
 }
