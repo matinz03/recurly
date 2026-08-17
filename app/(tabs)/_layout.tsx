04b0f4c767573ceed00ce8feb5bc50d5614cc1de
Matin <matinpsn1@gmail.com>
2026-08-17 12:28:33 +0200

    fix: update padding bottom for items row

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,8 @@
 import {Tabs, Redirect} from "expo-router";
 import {tabs} from "@/constants/data";
-import {View} from "react-native";
+import {View,Image} from "react-native";
 import { colors, components } from '@/constants/theme'
 import clsx from "clsx";
-import {Image} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useAuth } from '@clerk/expo';
 
